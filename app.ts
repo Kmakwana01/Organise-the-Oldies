9923be7ca871a509a05094b3245ef9713fbfcc73
Kmakwana01 <kmakwana1255@gmail.com>
2024-12-20 12:47:45 +0530

    some changes

--- app.ts
+++ app.ts
@@ -8,26 +8,26 @@ import cors from 'cors';
 import { verifyToken } from "./middleware/auth";
 
 require('dotenv').config()
-const mongodbPath:any = process.env.MONGO_URL;
+const mongodbPath: any = process.env.MONGO_URL || "mongodb://localhost:27017/OTO";
 
 mongoose.connect(mongodbPath)
   .then(() => console.log('Connected!'))
   .catch((err) => console.log(err.message))
 
-  import userRouter from './routes/userRoute';
-  import siblingAndGrandParentRouter from './routes/siblingAndGrandParentRoute';
-  import contactRouter from './routes/contactRoute';
-  import keyInfoRouter from './routes/keyInfoRoute';
-  import eventRouter from './routes/eventRoute';
-  import notificationRouter from './routes/notificationRoute';
-  import reminderRouter from './routes/reminderRoute';
-  import medicinesRouter from './routes/medicinesRoute';
-  import socketRouter from './routes/socketRoute';
-  import homePageRouter from './routes/homePageRoute';
-  import subscriptionRouter from './routes/subscriptionRoute'
+import userRouter from './routes/userRoute';
+import siblingAndGrandParentRouter from './routes/siblingAndGrandParentRoute';
+import contactRouter from './routes/contactRoute';
+import keyInfoRouter from './routes/keyInfoRoute';
+import eventRouter from './routes/eventRoute';
+import notificationRouter from './routes/notificationRoute';
+import reminderRouter from './routes/reminderRoute';
+import medicinesRouter from './routes/medicinesRoute';
+import socketRouter from './routes/socketRoute';
+import homePageRouter from './routes/homePageRoute';
+import subscriptionRouter from './routes/subscriptionRoute'
 
-  import sendMailUserRouter from './routes/sendMailRouteUser';
-  import medicinesTakenRouter from './routes/medicinesTakenRoute';
+import sendMailUserRouter from './routes/sendMailRouteUser';
+import medicinesTakenRouter from './routes/medicinesTakenRoute';
 
 const app = express();
 
@@ -36,7 +36,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(cors(),express.json());
+app.use(cors(), express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -44,19 +44,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/audio', verifyToken, express.static(path.join(__dirname, 'public/audio')));
 
 
-app.use('/' , userRouter);
-app.use('/siblingAndGrandParent' , siblingAndGrandParentRouter);
-app.use('/contact' , contactRouter);
-app.use('/keyInfo' , keyInfoRouter);
-app.use('/event' , eventRouter);
-app.use('/notification' , notificationRouter);
-app.use('/reminder' , reminderRouter);
-app.use('/medicines' , medicinesRouter);
-app.use('/chat' , socketRouter);
-app.use('/homePage' , homePageRouter);
+app.use('/', userRouter);
+app.use('/siblingAndGrandParent', siblingAndGrandParentRouter);
+app.use('/contact', contactRouter);
+app.use('/keyInfo', keyInfoRouter);
+app.use('/event', eventRouter);
+app.use('/notification', notificationRouter);
+app.use('/reminder', reminderRouter);
+app.use('/medicines', medicinesRouter);
+app.use('/chat', socketRouter);
+app.use('/homePage', homePageRouter);
 app.use('/subscription', subscriptionRouter);
-app.use('/sendMailUser' , sendMailUserRouter);
-app.use('/medicinesTaken' , medicinesTakenRouter);
+app.use('/sendMailUser', sendMailUserRouter);
+app.use('/medicinesTaken', medicinesTakenRouter);
 
 import cron from 'node-cron';
 import { trashFileRemove } from './controller/cronJobController';
