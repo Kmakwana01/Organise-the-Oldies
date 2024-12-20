ea581e61509bc6c25aff1cc28cee71fa89f54a5c
Kmakwana01 <kmakwana1255@gmail.com>
2024-12-20 15:12:54 +0530

    db connect

--- app.ts
+++ app.ts
@@ -8,11 +8,34 @@ import cors from 'cors';
 import { verifyToken } from "./middleware/auth";
 
 require('dotenv').config()
-const mongodbPath: any = process.env.MONGO_URL || "mongodb+srv://kmakwana1255:kmakwana@cluster1.hfoy8xo.mongodb.net/test?retryWrites=true&w=majority";
+// const mongodbPath: any = process.env.MONGO_URL || "mongodb+srv://kmakwana1255:kmakwana@cluster1.hfoy8xo.mongodb.net/test?retryWrites=true&w=majority";
 
-mongoose.connect(mongodbPath || "mongodb+srv://kmakwana1255:kmakwana@cluster1.hfoy8xo.mongodb.net/test?retryWrites=true&w=majority", {})
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch(error => console.error('Error connecting to MongoDB:', error.message));
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kmakwana1255:kmakwana@cluster1.hfoy8xo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 import userRouter from './routes/userRoute';
 import siblingAndGrandParentRouter from './routes/siblingAndGrandParentRoute';
